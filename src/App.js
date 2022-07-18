d92d7204cf14a7ec957d4a971b2cd8849b3cbe51
acfier <acfier@gmail.com>
2022-07-18 19:46:46 -0400

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"; 
 import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
